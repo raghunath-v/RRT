4b6d073805b644c58f6b66fd027e343e390088ad
buttlet <atlisigurgeirsson@gmail.com>
2018-01-21 00:45:34 +0100

    cleanup

--- RRT.py
+++ RRT.py
@@ -1,8 +1,8 @@
-from graphics import *
-from collections import defaultdict
-import numpy as np
+"""An implementation of the RRT algorithm in python"""
 import random
-import sys
+from collections import defaultdict
+from graphics import *
+import numpy as np
 
 class Graph:
     """Simple graph class"""
@@ -23,8 +23,8 @@ class Graph:
         self._graph[node_1].add(node_2)
         if not self._directed:
             self._graph[node_2].add(node_1)
-        self.set_area([node_1,node_2])
-    
+        self.set_area([node_1, node_2])
+
     def set_area(self, nodes):
         """Sets the bounding box of graph"""
         for node in nodes:
@@ -42,7 +42,7 @@ class Graph:
         min_dist = np.sqrt(self._bottom**2 + self._right**2)
         contender = None
         for neighbor in self._graph:
-            if node.dist_to(neighbor)<min_dist:
+            if node.dist_to(neighbor) < min_dist:
                 contender = neighbor
                 min_dist = node.dist_to(contender)
         self.add(node, contender)
@@ -55,15 +55,14 @@ class Graph:
         min_dist = np.sqrt(self._bottom**2 + self._right**2)
         contender = None
         for neighbor in self._graph:
-            if node.dist_to(neighbor)<min_dist:
+            if node.dist_to(neighbor) < min_dist:
                 contender = neighbor
                 min_dist = node.dist_to(contender)
-         
         self.add(contender.get_close(node, delta_q), contender)
 
     def remove(self, node):
         """ Remove all references to a node"""
-        for n, edges in self._graph.items():
+        for ind, edges in self._graph.items():
             try:
                 edges.remove(node)
             except KeyError:
@@ -86,12 +85,12 @@ class Graph:
                 if new_path:
                     return new_path
         return None
-    
+
     def draw(self):
         """Draws the graph"""
         win = GraphWin("Graph", self._right+10, self._bottom+10)
         for node in self._graph:
-            curr_loc = Point(node.get_x(), node.get_y()) 
+            curr_loc = Point(node.get_x(), node.get_y())
             Circle(curr_loc, 5).draw(win)
             for neighbor in self._graph[node]:
                 if neighbor:
@@ -109,9 +108,11 @@ class Node:
         self._y = loc_y
 
     def get_x(self):
+        """Returns the x coordinate of the node"""
         return self._x
 
     def get_y(self):
+        """Returns the y coordinate of the node"""
         return self._y
 
     def get_close(self, node, delta_q):
@@ -120,26 +121,27 @@ class Node:
             between this node and the input node in
             a delta_q distance from this node
         """
-        # distance between the two nodes
-        D = np.sqrt((self._x - node.get_x())**2 + (self._y - node.get_y())**2 )
-        new_x = self._x + (delta_q/D)*(node.get_x() - self._x)
-        new_y = self._y + (delta_q/D)*(node.get_y() - self._y)
+        # Dist is the distance between the two nodes
+        dist = self.dist_to(node)
+        new_x = self._x + (delta_q/dist)*(node.get_x() - self._x)
+        new_y = self._y + (delta_q/dist)*(node.get_y() - self._y)
         return Node(new_x, new_y)
 
     def dist_to(self, node):
+        """Returns the distance from this node to another"""
         return np.sqrt((self._x - node.get_x())**2 + (self._y - node.get_y())**2)
 
     def __repr__(self):
         return "( " + str(self._x) + ", " + str(self._y) + " )"
 
 def build_rrt(q_init, K, delta_q):
+    """Builds an RRT"""
     g = Graph()
     g.add_node(q_init)
 
     for k in range(K):
-        q_rand = Node(random.uniform(0,1000), random.uniform(0,1000))
+        q_rand = Node(random.uniform(0, 1000), random.uniform(0, 1000))
         g.add_between_nearest(q_rand, delta_q)
-    
     g.draw()
 
-build_rrt(Node(500,500), 1000, 20)
+build_rrt(Node(500, 500), 1000, 20)
